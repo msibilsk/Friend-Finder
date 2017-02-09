088ad8098b90f486b1c50e13e231ff1707875a75
Mary Willis <msibilsk@gmail.com>
2017-02-09 15:55:36 -0600

    everything except i need a check to make sure they filled out the form

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+var friendArray = [
+  {
+    "name": "Luigi",
+    "photo": "https://upload.wikimedia.org/wikipedia/en/f/f1/LuigiNSMBW.png",
+    "scores": [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+  },
+    {
+    "name": "Mario",
+    "photo": "https://upload.wikimedia.org/wikipedia/en/9/99/MarioSMBW.png",
+    "scores": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  },
+    {
+    "name": "Peach",
+    "photo": "https://www.mariowiki.com/images/thumb/b/b5/Peach_-_Mario_Party_10.png/180px-Peach_-_Mario_Party_10.png",
+    "scores": [2, 3, 2, 3, 2, 3, 3, 2, 2, 2]
+  },
+    {
+    "name": "Yoshi",
+    "photo": "https://www.mariowiki.com/images/thumb/9/93/Yoshi_FS.png/180px-Yoshi_FS.png",
+    "scores": [5, 1, 5, 1, 5, 1, 1, 5, 5, 1]
+  }
+];
+
+module.exports = friendArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,14 @@
+var friends = require("../data/friends.js");
 
+module.exports = function(app) {
 
-module.exports = function(app) {};
+    app.get("/api/friends", function(req, res) {
+        res.json(friends);
+    });
+
+    app.post("/api/friends", function(req, res) {
+        friends.push(req.body);
+        res.json(true);
+    });
+
+};
